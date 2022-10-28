04c684c96f883ee82389ba81a512b7f635d5b215
bia andersson <tobias.andersson@comeon.com>
2022-10-28 17:08:32 +0200

    separated the code to production and staging branches

--- live/stage/data/mysql/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "address" {
-  value       = aws_db_instance.example.address
-  description = "The address of the RDS instance."
-}
-
-output "port" {
-  value       = aws_db_instance.example.port
-  description = "The port on which the DB accepts connections."
-}
