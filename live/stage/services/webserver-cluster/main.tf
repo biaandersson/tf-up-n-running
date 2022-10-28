72cd9b88c60a044b55dcbc5fe1f436d9a55ff71f
bia andersson <tobias.andersson@comeon.com>
2022-10-28 18:35:35 +0200

    added git source for the module to be used

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -1,12 +1,10 @@
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/biaandersson/tf-up-n-running/tree/staging/modules/services/webserver-cluster"
 
   # Required parameters
-  db_remote_state_bucket = var.db_remote_state_bucket
-  db_remote_state_key    = var.db_remote_state_key
-
-  # Optional parameters
-  cluster_name = var.cluster_name
+  db_remote_state_bucket = "(bucket name)"
+  db_remote_state_key    = "stage/database/mysql/terraform.tfstate"
+  cluster_name           = "webserver-cluster-staging"
 
   instance_type = "t2.micro"
   min_size      = 2
@@ -14,9 +12,9 @@ module "webserver_cluster" {
 }
 
 resource "aws_security_group_rule" "allow_testing_inbound" {
-  type              = "ingress"
   security_group_id = module.webserver_cluster.alb_security_group_id
 
+  type        = "ingress"
   from_port   = 1234
   to_port     = 1234
   protocol    = "tcp"
